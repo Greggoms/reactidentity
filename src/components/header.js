9ed1b07a4e7130787786c5fed443306bfe5ea3b2
Greg Burton <gpburton@ualr.edu>
2021-12-09 12:04:23 -0600

    logout button is now conditional

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,46 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import { useIdentityContext } from "react-netlify-identity"
 import { Nav } from "./Nav"
 import { Logout } from "../utils/Logout"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+const Header = ({ siteTitle }) => {
+  const { isLoggedIn } = useIdentityContext()
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-        display: "flex",
-        alignItems: "flex-end",
-        justifyContent: "space-between",
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-      <Nav />
-      <Logout />
-    </div>
-  </header>
-)
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+          display: "flex",
+          alignItems: "flex-end",
+          justifyContent: "space-between",
+        }}
+      >
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+        <Nav />
+        {isLoggedIn && <Logout />}
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
